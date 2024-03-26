58720e159ae0c5eb8aaae208bf83c1cb9fe2f2db
Joan-Abreu <joan.abreu2007@gmail.com>
2024-03-26 11:22:58 -0400

    Add dynamic pages

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -10,11 +10,11 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 class CarMake(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
+    test = models.TextField()
     # Other fields as needed
 
     def __str__(self):
         return self.name  # Return the name as the string representation
-
 
 class CarModel(models.Model):
     car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)  # Many-to-One relationship
